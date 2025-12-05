6d4fa350bcd97881e4a95be7ac3dd7df2453030a
UdanDias <udandias892@gmail.com>
2025-12-05 13:36:01 +0530

    created health check

---
+++ src/main/java/lk/HotelMgmt/controller/AdminController.java
@@ -0,0 +1,8 @@
+package lk.HotelMgmt.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AdminController {
+
+}
---
+++ src/main/java/lk/HotelMgmt/controller/Health.java
@@ -0,0 +1,14 @@
+package lk.HotelMgmt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/health")
+public class Health {
+    @GetMapping("/health")
+    public String health(){
+        return "Health check is running";
+    }
+}
