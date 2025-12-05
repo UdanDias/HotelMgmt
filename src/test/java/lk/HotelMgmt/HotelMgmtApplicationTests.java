fa86872717ffcb216cf0f79b8d76dce9684fc4fe
UdanDias <udandias892@gmail.com>
2025-12-05 11:49:23 +0530

    initial commit

---
+++ src/test/java/lk/HotelMgmt/HotelMgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.HotelMgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
