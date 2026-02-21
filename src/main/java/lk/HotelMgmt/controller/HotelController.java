ea4851a3cadd73b16519c03c8739f42e2c6a820c
Oshini <oshinivithanage1003@gmail.com>
2026-02-21 21:54:26 +0530

    Add hotel controller

--- src/main/java/lk/HotelMgmt/controller/HotelController.java
+++ src/main/java/lk/HotelMgmt/controller/HotelController.java
@@ -1,4 +1,42 @@
 package lk.HotelMgmt.controller;
 
+import lk.HotelMgmt.dto.HotelDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
 public class HotelController {
+    public ResponseEntity<Void> addHotel(HotelDTO hotelDTO){
+        if(hotelDTO == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<>(HttpStatus.CREATED);
+
+    }
+
+    public ResponseEntity<Void> deleteHotel(String hotelId){
+        if(hotelId == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+
+    public ResponseEntity<Void> updateHotel(String hotelId, HotelDTO hotelDTO){
+        if(hotelDTO == null || hotelId == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+
+    public ResponseEntity<HotelDTO> getSelectedHotel(String hotelId ){
+        if(hotelId == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        return null;
+    }
+
+    public ResponseEntity<List<HotelDTO>> getAllHotels(){
+        return null;
+    }
 }
