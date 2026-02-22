6baf6af824fdae2543943199772bedc02f9ccf89
UdanDias <udandias892@gmail.com>
2026-02-22 13:46:13 +0530

    Created room DTO

--- src/main/java/lk/HotelMgmt/dto/RoomDTO.java
+++ src/main/java/lk/HotelMgmt/dto/RoomDTO.java
@@ -1,4 +1,20 @@
 package lk.HotelMgmt.dto;
 
-public class RoomDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RoomDTO implements Serializable {
+    private Long roomId;
+    private Long hotelId;
+    private String roomNo;
+    private String description;
+    private String roomType;
+    private boolean isBooked;
+    private int capacity;
+    private double perDayPrice;
 }
