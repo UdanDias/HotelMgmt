8774f77807bb1a91deab87cdfbe8d14fd63ca0bc
UdanDias <udandias892@gmail.com>
2026-03-03 20:29:59 +0530

    Created CustomerDao

---
+++ src/main/java/lk/HotelMgmt/dao/CustomerDao.java
@@ -0,0 +1,9 @@
+package lk.HotelMgmt.dao;
+
+import lk.HotelMgmt.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerDao extends JpaRepository<CustomerEntity, String> {
+}
