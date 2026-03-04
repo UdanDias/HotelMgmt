3433ff7414d7a617b24fa70db76a96701bcc3732
UdanDias <udandias892@gmail.com>
2026-03-04 23:30:44 +0530

    Created add method for User entity

--- src/main/java/lk/HotelMgmt/service/impl/UserServiceImpl.java
+++ src/main/java/lk/HotelMgmt/service/impl/UserServiceImpl.java
@@ -1,18 +1,31 @@
 package lk.HotelMgmt.service.impl;
 
+import jakarta.transaction.Transactional;
+import lk.HotelMgmt.dao.UserDao;
 import lk.HotelMgmt.dto.AdminDTO;
 import lk.HotelMgmt.dto.UserDTO;
+import lk.HotelMgmt.entity.UserEntity;
 import lk.HotelMgmt.service.UserService;
+import lk.HotelMgmt.util.EntityDTOConvert;
 import lk.HotelMgmt.util.UtilData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+    private final EntityDTOConvert entityDTOConvert;
+
+
     @Override
     public void addUser(UserDTO userDTO) {
         userDTO.setUserId(UtilData.generateUserId());
+        var userEntity = entityDTOConvert.convertUserDTOToUserEntity(userDTO);
+        userDao.save(userEntity);
     }
 
     @Override
