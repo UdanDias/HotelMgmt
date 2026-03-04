6936726ae246acb686d0d4429eda3cde51d9b957
UdanDias <udandias892@gmail.com>
2026-03-04 23:24:24 +0530

    Created custom query for getting availabale rooms

--- src/main/java/lk/HotelMgmt/dao/RoomDao.java
+++ src/main/java/lk/HotelMgmt/dao/RoomDao.java
@@ -2,8 +2,13 @@ package lk.HotelMgmt.dao;
 
 import lk.HotelMgmt.entity.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RoomDao extends JpaRepository<RoomEntity, String> {
+    @Query("SELECT r FROM RoomEntity r WHERE r.isBooked = false")
+    List<RoomEntity> findAvailableRooms();
 }
