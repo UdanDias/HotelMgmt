4a72b6899daf2c667fd2b7fdbfcbc6a07d6eb584
UdanDias <udandias892@gmail.com>
2026-03-05 23:06:39 +0530

    created Customer add method

--- src/main/java/lk/HotelMgmt/service/impl/CustomerServiceImpl.java
+++ src/main/java/lk/HotelMgmt/service/impl/CustomerServiceImpl.java
@@ -1,17 +1,24 @@
 package lk.HotelMgmt.service.impl;
 
+import lk.HotelMgmt.dao.CustomerDao;
 import lk.HotelMgmt.dto.CustomerDTO;
+import lk.HotelMgmt.entity.CustomerEntity;
 import lk.HotelMgmt.service.CustomerService;
 import lk.HotelMgmt.util.UtilData;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class CustomerServiceImpl implements CustomerService {
+    private final CustomerDao customerDao;
     @Override
     public void addCustomer(CustomerDTO customerDTO) {
         customerDTO.setCustId(UtilData.generateCustomerId());
+        CustomerEntity customerEntity=customerDao.findById(customerDTO.getCustId()).orElseThrow(()->new RuntimeException("Customer Not Found"));
+        customerDao.save(customerEntity);
     }
 
     @Override
